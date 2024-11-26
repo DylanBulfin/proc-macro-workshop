a32612f2f13544ba93c281fa20b7023823743113
Dylan Bulfin <dylanbulfin@gmail.com>
2024-11-26 17:09:49 -0500

    Fix each attribute

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,7 @@
-use core::slice::SlicePattern;
 use std::iter::Map;
 
 use proc_macro2::TokenStream;
-use quote::{format_ident, quote, quote_spanned};
+use quote::{format_ident, quote, quote_spanned, ToTokens};
 use syn::{
     braced,
     parse::{Parse, ParseStream},
@@ -10,8 +9,9 @@ use syn::{
     punctuated::Punctuated,
     spanned::Spanned,
     token::Brace,
-    AngleBracketedGenericArguments, Attribute, Data, DataStruct, DeriveInput, Fields,
-    GenericArgument, Ident, Path, PathArguments, PathSegment, Token, Type, TypePath, Visibility,
+    AngleBracketedGenericArguments, Attribute, Data, DataStruct, DeriveInput, Expr, Fields,
+    GenericArgument, Ident, LitStr, Path, PathArguments, PathSegment, Token, Type, TypePath,
+    Visibility,
 };
 
 struct StructItem {
@@ -24,7 +24,6 @@ struct StructItem {
 }
 
 struct StructField {
-    attr: Vec<Attribute>,
     //_vis: Visibility,
     ident: Ident,
     //_colon: Token![:],
@@ -36,7 +35,12 @@ struct StructField {
     // options we store the inside of the Option instead. E.g. for type `String` it is
     // String, and for type `Option<Vec<String>>` it is `Vec<String>`
     ty: Type,
+
+    // If this struct contains a
+    each_attr: Option<EachAttr>,
 }
+
+struct EachAttr(LitStr);
 
 impl Parse for StructItem {
     fn parse(input: ParseStream) -> syn::Result<Self> {
@@ -55,31 +59,55 @@ impl Parse for StructItem {
 
 impl Parse for StructField {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let attr = input.call(Attribute::parse_outer)?;
+        let attrs = input.call(Attribute::parse_outer)?;
         let _vis: Visibility = input.parse()?;
         let ident = input.parse()?;
         let _colon: Token![:] = input.parse()?;
         let ty = input.parse()?;
 
+        let each_attr = if let Some(a) = attrs.into_iter().find(|a| a.path().is_ident("builder")) {
+            let res: EachAttr = a.parse_args()?;
+            Some(res)
+        } else {
+            None
+        };
+
         if let Some(inner_ty) = check_field_optional(&ty) {
             Ok(Self {
-                attr,
                 ident,
                 ty: inner_ty,
                 opt: true,
+                each_attr,
             })
         } else {
             Ok(Self {
-                attr,
                 ident,
                 ty,
                 opt: false,
+                each_attr,
             })
         }
     }
 }
 
-#[proc_macro_derive(Builder)]
+impl Parse for EachAttr {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let lhs: Ident = input.parse()?;
+        let _equal: Token![=] = input.parse()?;
+        let lit: LitStr = input.parse()?;
+
+        if lhs == "each" {
+            Ok(Self(lit))
+        } else {
+            Err(syn::Error::new(
+                lhs.span(),
+                format!("Expected attr 'each', found {}", lhs),
+            ))
+        }
+    }
+}
+
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let item = parse_macro_input!(input as StructItem);
 
@@ -90,8 +118,11 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 
     let builder_def = builder_definition(&builder_ident, &item.vis, field_vec.as_slice());
     let builder_impl = builder_impl(&ident, &builder_ident, field_vec.as_slice());
+    let base_impl = base_impl(&ident, &builder_ident, field_vec.as_slice());
 
     quote! {
+        #base_impl
+
         #builder_impl
 
         #builder_def
@@ -103,6 +134,31 @@ fn base_impl<'a, I>(ident: &Ident, builder_ident: &Ident, fields: I) -> TokenStr
 where
     I: IntoIterator<Item = &'a StructField>,
 {
+    let builder_fields = fields.into_iter().map(|field| {
+        let id = &field.ident;
+
+        if field.each_attr.is_some() {
+            quote! {
+                #id: Some(Vec::new())
+            }
+        } else {
+            quote! {
+                #id: None
+            }
+        }
+    });
+
+    quote! {
+        impl #ident {
+            fn builder() -> #builder_ident {
+                #builder_ident {
+                    #(
+                        #builder_fields
+                    ),*
+                }
+            }
+        }
+    }
 }
 
 fn builder_impl<'a, I>(ident: &Ident, builder_ident: &Ident, fields: I) -> TokenStream
@@ -110,21 +166,69 @@ where
     I: IntoIterator<Item = &'a StructField>,
 {
     let mut setters: Vec<_> = Vec::new();
+    let mut field_checks: Vec<_> = Vec::new();
     let mut field_inits: Vec<_> = Vec::new();
+    let mut each_funcs: Vec<_> = Vec::new();
 
-    for f in fields {
-        let ty = &f.ty;
-        let id = &f.ident;
+    for field in fields {
+        let ty = &field.ty;
+        let id = &field.ident;
+        let opt = field.opt;
 
-        setters.push(quote! {
-            pub fn #id(&mut self, #id: #ty) -> &mut Self {
-                self.#id = Some(#ty);
+        if let Some(EachAttr(val)) = &field.each_attr {
+            let each_id = Ident::new(val.value().as_str(), ty.span());
+            let inner_ty = get_vec_inner_type(ty);
+
+            if &each_id != id {
+                setters.push(quote! {
+                    pub fn #id(&mut self, #id: #ty) -> &mut Self {
+                        self.#id = Some(#id);
+                        self
+                    }
+                });
             }
-        });
 
-        field_inits.push(quote! {
-            #ty: None
-        });
+            each_funcs.push(quote! {
+                pub fn #each_id(&mut self, #each_id: #inner_ty) -> &mut Self {
+                    match self.#id {
+                        Some(ref mut v) => v.push(#each_id),
+                        None => {
+                            let mut new_val = Vec::new();
+                            new_val.push(#each_id);
+
+                            self.#id = Some(new_val);
+                        }
+                    }
+
+                    self
+                }
+            });
+        } else {
+            setters.push(quote! {
+                pub fn #id(&mut self, #id: #ty) -> &mut Self {
+                    self.#id = Some(#id);
+                    self
+                }
+            });
+        }
+
+        if !opt {
+            field_checks.push(quote! {
+                if self.#id.is_none() {
+                    return Err(format!("Field {} is unexpectedly None", stringify!(#id)).into());
+                }
+            })
+        }
+
+        if opt {
+            field_inits.push(quote! {
+                #id: self.#id.clone()
+            })
+        } else {
+            field_inits.push(quote! {
+                #id: self.#id.clone().unwrap()
+            });
+        }
     }
 
     quote! {
@@ -133,10 +237,22 @@ where
                 #setters
             )*
 
-            pub fn build(&mut self) -> Result<#ident, Box<dyn Error>> {
+            #(
+                #each_funcs
+            )*
+
+            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
                 #(
-                    #field_inits
-                ),*
+                    #field_checks
+                )*
+
+                Ok(
+                    #ident {
+                        #(
+                            #field_inits
+                        ),*
+                    }
+                )
             }
         }
     }
@@ -166,27 +282,29 @@ where
     }
 }
 
-//     Type::Path(
-//         TypePath {
-//             qself: None,
-//             path: Path {
-//                 segments: [
-//                     PathSegment {
-//                         ident: "Option",
-//                         arguments: PathArguments::AngleBracketed(
-//                             AngleBracketedGenericArguments {
-//                                 args: [
-//                                     GenericArgument::Type(
-//                                         ...
-//                                     ),
-//                                 ],
-//                             },
-//                         ),
-//                     },
-//                 ],
-//             },
-//         },
-//     )
+fn get_vec_inner_type(ty: &Type) -> &Type {
+    if let Type::Path(TypePath {
+        qself: None,
+        path: Path { segments, .. },
+        ..
+    }) = ty
+    {
+        if segments.len() == 1 && segments[0].ident == "Vec" {
+            if let PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }) =
+                &segments[0].arguments
+            {
+                if args.len() == 1 {
+                    if let GenericArgument::Type(t) = &args[0] {
+                        return t;
+                    }
+                }
+            }
+        }
+    }
+
+    panic!("Can't use each attribute on non-Vec fields")
+}
+
 fn check_field_optional(ty: &Type) -> Option<Type> {
     if let Type::Path(TypePath {
         qself: None,
