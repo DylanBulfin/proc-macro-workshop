fb09bac5fe6fe580da1ef6165035a3dc42b7be46
Dylan Bulfin <dylanbulfin@gmail.com>
2024-11-26 08:09:56 -0500

    Saving progress

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,139 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote, quote_spanned};
+use syn::{
+    parse_macro_input, spanned::Spanned, AngleBracketedGenericArguments, Data, DataStruct,
+    DeriveInput, Fields, GenericArgument, Ident, Path, PathArguments, PathSegment, Type, TypePath,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let tree = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let ident = &tree.ident;
+    let builder_ident = format_ident!("{}Builder", ident);
+
+    let mut arg_id = Vec::new();
+    let mut arg_ty = Vec::new();
+    let mut arg_op = Vec::new();
+
+    match tree.data {
+        Data::Struct(data) => match data.fields {
+            Fields::Named(fields) => {
+                for f in fields.named {
+                    arg_id.push(f.ident);
+
+                    let (ty, opt) = try_strip_arg(f.ty);
+                    arg_ty.push(ty);
+                    arg_op.push(opt);
+                }
+            }
+            Fields::Unnamed(_) | Fields::Unit => unimplemented!(),
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    };
+
+    let required_check = quote! {
+        #(
+            if !#arg_op && self.#arg_id.is_none() {
+                return Err(format!("Field {} is None", stringify!(#arg_id)).into());
+            }
+        )*
+    };
+
+    let constructor = quote! {
+        #ident {
+            #(
+                #arg_id: if #arg_op {self.#arg_id} else {self.#arg_id.take().unwrap()}
+            ),*
+        }
+    };
+
+    quote! {
+        pub struct #builder_ident {
+            #(
+                #arg_id: Option<#arg_ty>
+            ),*
+        }
+
+        impl #builder_ident {
+            #(
+                pub fn #arg_id(&mut self, #arg_id: #arg_ty) -> &mut Self {
+                    self.#arg_id = Some(#arg_id);
+                    self
+                }
+            )*
+
+            pub fn build(&mut self) -> Result<#ident, Box<dyn std::error::Error>> {
+                #required_check
+
+                Ok(
+                    #ident {
+                        #(
+                            #arg_id: if #arg_op {self.#arg_id} else {self.#arg_id.take().unwrap()}
+                        ),*
+                    }
+                )
+            }
+        }
+
+        impl #ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    #(
+                        #arg_id: None
+                    ),*
+                }
+            }
+        }
+    }
+    .into()
+}
+
+//     Type::Path(
+//         TypePath {
+//             qself: None,
+//             path: Path {
+//                 segments: [
+//                     PathSegment {
+//                         ident: "Option",
+//                         arguments: PathArguments::AngleBracketed(
+//                             AngleBracketedGenericArguments {
+//                                 args: [
+//                                     GenericArgument::Type(
+//                                         ...
+//                                     ),
+//                                 ],
+//                             },
+//                         ),
+//                     },
+//                 ],
+//             },
+//         },
+//     )
+
+fn try_strip_arg(arg_ty: Type) -> (Type, bool) {
+    'outer: {
+        if let Type::Path(TypePath {
+            qself: None,
+            path: Path { ref segments, .. },
+            ..
+        }) = arg_ty
+        {
+            if segments.len() == 1 && segments[0].ident == "Option" {
+                if let PathArguments::AngleBracketed(AngleBracketedGenericArguments {
+                    ref args,
+                    ..
+                }) = segments[0].arguments
+                {
+                    if args.len() == 1 {
+                        if let GenericArgument::Type(ref t) = args[0] {
+                            break 'outer (t.to_owned(), true);
+                        }
+                    }
+                }
+            }
+        }
+
+        break 'outer (arg_ty, false);
+    }
 }
